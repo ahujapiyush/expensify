66f200c557115d86de3d4dfe8be1ff547ee4cc42
PIYUSH AHUJA <PIYUSH.AH@MAGUREINC.COM>
2023-06-26 12:23:02 +0530

    Tutorial 2

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import AppNavigation from "./navigation/AppNavigation.js";
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <AppNavigation />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+}
+export default HomeScreen;
